948981ba01bcaf71c11ba1010d6bef6b27d1d476
Petr Trofimov <petrofimov@yandex.ru>
2012-12-11 00:09:11 +0300

    simple alg

--- test.php
+++ test.php
@@ -1,2 +1,30 @@
 <?php
 
+class Processor
+{
+	private $in, $out;
+
+	public function process($text)
+	{
+		//$lines = explode(PHP_EOL, $text);
+		//$new = array_replace($lines, array(''), array('</p><p>'));
+		//$out = implode(PHP_EOL, $new);
+		$out = preg_replace('/('.PHP_EOL.')+/', '</p><p>', $text);
+		$out = preg_replace('/(\S+)\.(\w+)/', '<span class="$2">$1</span>', $out);
+		$out = preg_replace('/<p>.*\.(\w+)<\/p>/', '<div class="$2">$1</div>', $out);
+
+		return '<p>'.$out.'</p>';
+	}
+}
+
+$text = <<<TEXT
+some word.b in sentence .l
+welcome
+welcome
+
+text
+TEXT;
+
+echo (new Processor)->process($text);
+
+// several .bold words.b in sentence
