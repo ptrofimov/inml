0562cd457ecb189979484efcfed54c98f1ccfeff
Petr Trofimov <petrofimov@yandex.ru>
2012-12-16 21:30:05 +0300

    wrote docs for Render\Html class

--- src/Inml/Render/Html.php
+++ src/Inml/Render/Html.php
@@ -6,10 +6,24 @@ use \Inml\Text\Paragraph;
 use \Inml\Text\Line;
 use \Inml\Text\Word;
 
+/**
+ * Class to render Inml\Text into HTML format
+ *
+ * @author Petr Trofimov <petrofimov@yandex.ru>
+ */
 class Html implements \Inml\Render
 {
+    /**
+     * Delimiter between classes, lines and words
+     */
     const CHAR_SPACE = ' ';
 
+    /**
+     * Renders Inml\Text into HTML format
+     *
+     * @param \Inml\Text $text Text to render
+     * @return string
+     */
     public function render(Text $text)
     {
         $out = '';
@@ -20,11 +34,23 @@ class Html implements \Inml\Render
         return $out;
     }
 
+    /**
+     * Joins array of styles into string
+     *
+     * @param $object
+     * @return string
+     */
     private function getClass($object)
     {
         return implode(self::CHAR_SPACE, $object->getStyles());
     }
 
+    /**
+     * Renders Paragraph object into HTML format
+     *
+     * @param \Inml\Text\Paragraph $paragraph
+     * @return string
+     */
     private function renderParagraph(Paragraph $paragraph)
     {
         if ($paragraph->hasStyles()) {
@@ -41,6 +67,12 @@ class Html implements \Inml\Render
         return $out . '</p>';
     }
 
+    /**
+     * Renders Line object into HTML format
+     *
+     * @param \Inml\Text\Line $line
+     * @return string
+     */
     private function renderLine(Line $line)
     {
         $words = [];
@@ -56,6 +88,12 @@ class Html implements \Inml\Render
         return $out;
     }
 
+    /**
+     * Renders Word object into HTML format
+     *
+     * @param \Inml\Text\Word $word
+     * @return string
+     */
     private function renderWord(Word $word)
     {
         $out = $word->getWord();
