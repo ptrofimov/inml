a29f9ae53f8623515cd48d6d6149b33ca2715e0c
Petr Trofimov <petrofimov@yandex.ru>
2012-12-16 22:11:20 +0300

    removed old files

--- examples/render.php
+++ examples/render.php
@@ -1,15 +1,18 @@
 <?php
 /**
- * Example script to demonstrate the usage of Inml class
+ * Example script to demonstrate the usage
  *
  * @author Petr Trofimov <petrofimov@yandex.ru>
  */
-require_once(dirname(__DIR__) . '/classes/Inml.php');
+require_once(dirname(__DIR__) . '/vendor/autoload.php');
+
+use \Inml\Text;
+use \Inml\Render\Html;
 
 $inml = file_get_contents(__DIR__ . '/example.inml');
 $styles = file_get_contents(__DIR__ . '/styles.css');
 
-$html = (new Inml)->render($inml);
+$html = (new Html)->render(new Text($inml));
 
 file_put_contents(__DIR__ . '/example.html',
     '<style>' . $styles . '</style>' . PHP_EOL . $html);
